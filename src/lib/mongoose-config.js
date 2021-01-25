b578e986981dc71445402ca86d0cd94bf0c22531
RipDevil <tiberi64@yandex.ru>
2021-01-25 17:35:16 +0700

    :compter: useCreateIndex warning fix

--- src/lib/mongoose-config.js
+++ src/lib/mongoose-config.js
@@ -3,7 +3,12 @@ const { mongoUri } = require('./config');
 
 module.exports = () => {
   mongoose
-    .connect(mongoUri, { useFindAndModify: false, useNewUrlParser: true, useUnifiedTopology: true }) // not using useNewUrlParser is deprecated
+    .connect(mongoUri, {
+      useFindAndModify: false,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    }) // not using useNewUrlParser is deprecated
     .then(() => console.log('Mongoose database has been connected'))
-    .catch(e => console.error(e));
+    .catch((e) => console.error(e));
 };
